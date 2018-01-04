82dae5a6bf89021eef19ce6345eeb4ea0643c0fc
timkuo25 <34412824+timkuo25@users.noreply.github.com>
2018-01-04 21:36:37 +0800

    event3

--- textgame_group.py
+++ textgame_group.py
@@ -1,66 +1,40 @@
-def chapter_2():
-	global x, goch2, par
-	
 	if par == 1:
+		theblock_for_changing_background = 13 + 1
 		if x == 0:
-			original_board("教室+對話框.png")
-		elif x == 4:
-			show_text_1("(過了一陣子)")
-		elif x == 4:
-			original_board("教室+對話框.png")
-			show_text_1("原以為上了大學脫離家裡，生活會改變許多。")			
-		elif x == 4:
-			show_text_2("其實也真的還好，或許個性上我就是比較獨立吧。")
-		elif x == 4:
-			original_board("教室+對話框.png")
-			show_text_1("不管怎麼樣，開學也快一個月了，差不多習慣了這裡的生活，")
-		elif x == 4:
-			original_board("教室+對話框.png")
-			show_text_1("系上的氣氛也漸漸活絡起來了。")
-		elif x == 4:
-			original_board("教室+好朋友+對話框+名字框.png")
-			show_text_name_for_4("神谷   智則")
-			show_text_1("周末有空嗎？要不要一起出去逛逛？")
-		elif x == 4:
 			original_board("小街+對話框.png")
-			show_text_1("開學以來多少也記住了幾位同學的名字，不過比較常往來的還是只有智則和凜。")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_1("智則是熱情的人，不過…該去嗎？")
 		elif x == 1:
+			show_text_1("這天吃完早餐準備去上課，")
+		elif x == 2:
+			show_text_2("走著走著，發現前方路上有張學生證。")
+		elif x == 3:
 			original_board("小街+對話框.png")
-			show_text_1("剛好沒甚麼事，就出去逛逛吧。")
-			show_text_2("在宿舍讀書好了。")
+			x += 1
 		elif x == 4:
+			show_text_1("B0X70XXXX，XXX，照片是個男生，從學號看應該和我一樣是管院的人。")
+		elif x == 5:
 			original_board("小街+對話框.png")
-			show_text_name_for_4("神谷   智則")
-			show_text_1("耐斯，那就到時候見囉！")
-		elif x == 4:
+			x += 1
+		elif x == 6:
+			show_text_1("這個嘛…我應該要…")
+			print(x)
+		elif x == 7:#button
 			original_board("小街+對話框.png")
-			show_text_1("要拒絕他還真有點不好意思，現在也沒什麼心情念書，出去散散心也好吧。")
-		elif x == 4:
+			x += 1
+		elif x == 8:
+			show_text_1("物歸原位(丟回去)")
+			show_text_2("先收著")
+			show_text_3("PO學校交流板")
+		elif x == 9:#選項一
+			original_board("小街+對話框.png")
+			x += 1
+		elif x == 10:
+			show_text_1("放著吧，這張學生證終究會回到屬於它的地方。")
+		elif x == 11:
+			original_board("小街+對話框.png")
+			x += 1
+		elif x == 12:
+			show_text_1("總之先進教室吧。")			
+		elif x == 13:
+			change_background("小街+對話框.png", "教室+對話框.png", 30)
 			par += 1
 			x = 0
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_name_for_4("神谷   智則")
-			show_text_1("好吧，我好像也應該讀點書……")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_name_for_4("神谷   智則")
-			show_text_1("沒有啦，我亂講的== 改天有空一起出來吧。")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_1("其實拒絕他還滿不好意思的，")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_1("不過也不確定自己是不是真的想和沒那麼熟的人出去。")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_1("該念的書還是得念吧，都進這間學校了")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_1("這個嘛…我應該要…")
-		elif x == 4:
-			original_board("小街+對話框.png")
-			show_text_1("這個嘛…我應該要…")
